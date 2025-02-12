9f446017ed7f24519daf15d1f840c8769bdf6d7e
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2025-02-12 16:55:32 -0500

    Update app.mjs

--- docs/app.mjs
+++ docs/app.mjs
@@ -6,6 +6,8 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 import * as Main from "./main.mjs";
 
 Main.hello();
+const initScript = document.getElementById("init");
+Main.window.initializeMessages(initScript.messageQueue);
 console.log(Function.prototype === Main.__Function__);
 const thisSource = new Main.Source(({ next, complete, error }) => {
   let i = 0;
