148e7c496f19458d1a45e497444b48841d3ce8d1
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2025-02-12 16:14:50 -0500

    Update WHATWG@20250212.mjs

--- docs/WHATWG@20250212.mjs
+++ docs/WHATWG@20250212.mjs
@@ -3,7 +3,14 @@
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
+import * as Iterators from "iterators";
+
 // If not cross-origin-isolated, the browser does not provide concurrent access to SharedArrayBuffers.
 // ECMAScript 2024 25.2.3
 export const SharedArrayBuffer = window.crossOriginIsolated ? globalThis.SharedArrayBuffer : () => { throw new Error("Only available in cross origin isolation."); };
 export const __SharedArrayBuffer__ = window.crossOriginIsolated ? globalThis.SharedArrayBuffer.prototype : null;
+
+class Window {
+  message: new Iterators.Source();
+}
+export window = new Window(self.window);
