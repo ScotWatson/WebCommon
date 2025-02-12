30dd6e15ba0b37ce08250a211418119276f7518b
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2025-02-12 13:42:44 -0500

    Update app.mjs

--- docs/app.mjs
+++ docs/app.mjs
@@ -6,17 +6,14 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 import * as App from "./modules.mjs";
 
 App.hello();
-const loadingIterators = import("https://scotwatson.github.io/WebCommon/20250212/iterators.mjs");
-loadingIterators.then((Iterators) => {
-  console.log(Function.prototype === Iterators.__Function__);
-  const thisSource = new Iterators.Source(({ next, complete, error }) => {
-    let i = 0;
-    setInterval(() => {
-      ++i;
-      next(i);
-    }, 500);
-  });
-  const thisSourceView = thisSource[Symbol.asyncIterator]();
-  const thisSink = new Iterators.Sink(console.log);
-  const thisStream = thisSink.stream(thisSource);
+console.log(Function.prototype === App.__Function__);
+const thisSource = new App.Source(({ next, complete, error }) => {
+  let i = 0;
+  setInterval(() => {
+    ++i;
+    next(i);
+  }, 500);
 });
+const thisSourceView = thisSource[Symbol.asyncIterator]();
+const thisSink = new App.Sink(console.log);
+const thisStream = thisSink.stream(thisSource);
