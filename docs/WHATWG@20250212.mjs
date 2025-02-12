33af4b18017d6600698d6e86ce769f83c0e2a98f
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2025-02-12 17:37:07 -0500

    Update WHATWG@20250212.mjs

--- docs/WHATWG@20250212.mjs
+++ docs/WHATWG@20250212.mjs
@@ -3,7 +3,7 @@
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-import * as Iterators from "iterators";
+import * as Streams from "streams";
 
 // If not cross-origin-isolated, the browser does not provide concurrent access to SharedArrayBuffers.
 // ECMAScript 2024 25.2.3
@@ -14,7 +14,7 @@ class Window {
   constructor() {
   }
   initializeMessages(queue) {
-    this.message = new Iterators.Source(({ next, complete, error }) => {
+    this.message = new Streams.Source(({ next, complete, error }) => {
       queue.addEventListener("message", next);
       queue.start();
     });
