6f66abd51e7786661f3b37b8562e2731c143de04
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2025-02-12 17:21:43 -0500

    Update window-init@20250212.js

--- docs/window-init@20250212.js
+++ docs/window-init@20250212.js
@@ -3,7 +3,9 @@
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-function insertScript(id, url) {
+const me = window.document.currentScript;
+
+me.insertScript = function insertScript(id, url) {
   const script = window.document.createElement("script");
   script.id = id;
   script.src = url;
@@ -13,7 +15,6 @@ function insertScript(id, url) {
 
 const MessageQueueScript = insertScript("MessageQueue", "https://scotwatson.github.io/WebCommon/MessageQueue@20250212.js");
 
-const me = window.document.currentScript;
 MessageQueueScript.addEventListener("load", () => {
 //  const MessageQueue = MessageQueueScript.exports.default;
   me.messageQueue = new window.MessageQueue(window);
