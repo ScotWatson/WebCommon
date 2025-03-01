0168dd317439bce2230deb1d1e49d60e3dce30a2
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2025-03-01 15:16:24 -0500

    Update sRGB@20250228.mjs

--- docs/sRGB@20250228.mjs
+++ docs/sRGB@20250228.mjs
@@ -32,16 +32,13 @@ export function createXYZBuffers(width, height) {
 }
 
 export function getRGBFromImage(imageData, result) {
-  for (let j = 0; j < imageData.height; ++j) {
-    for (let i = 0; i < imageData.width; ++i) {
-      const baseIndex = imageData.width * j + i;
-      const r_gamma = imageData.data[4 * baseIndex] / 255;
-      const g_gamma = imageData.data[4 * baseIndex + 1] / 255;
-      const b_gamma = imageData.data[4 * baseIndex + 2] / 255;
-      result.R[baseIndex] = gammaDecode(r_gamma);
-      result.G[baseIndex] = gammaDecode(g_gamma);
-      result.B[baseIndex] = gammaDecode(b_gamma);
-    }
+  for (let baseIndex = 0; baseIndex < imageData.height * imageData.width; ++baseIndex) {
+    const r_gamma = imageData.data[4 * baseIndex] / 255;
+    const g_gamma = imageData.data[4 * baseIndex + 1] / 255;
+    const b_gamma = imageData.data[4 * baseIndex + 2] / 255;
+    result.R[baseIndex] = gammaDecode(r_gamma);
+    result.G[baseIndex] = gammaDecode(g_gamma);
+    result.B[baseIndex] = gammaDecode(b_gamma);
   }
 }
 
@@ -54,18 +51,15 @@ export function getXYZFromRGB(rgb, result) {
 }
 
 export function getXYZFromImage(imageData, result) {
-  for (let j = 0; j < imageData.height; ++j) {
-    for (let i = 0; i < imageData.width; ++i) {
-      const baseIndex = imageData.width * j + i;
-      const r_gamma = imageData.data[4 * baseIndex] / 255;
-      const g_gamma = imageData.data[4 * baseIndex + 1] / 255;
-      const b_gamma = imageData.data[4 * baseIndex + 2] / 255;
-      const r_linear = gammaDecode(r_gamma);
-      const g_linear = gammaDecode(g_gamma);
-      const b_linear = gammaDecode(b_gamma);
-      result.X[baseIndex] = 0.4124 * r_linear + 0.3576 * g_linear + 0.1805 * b_linear;
-      result.Y[baseIndex] = 0.2126 * r_linear + 0.7152 * g_linear + 0.0722 * b_linear;
-      result.Z[baseIndex] = 0.0193 * r_linear + 0.1192 * g_linear + 0.9505 * b_linear;
-    }
+  for (let baseIndex = 0; baseIndex < imageData.height * imageData.width; ++baseIndex) {
+    const r_gamma = imageData.data[4 * baseIndex] / 255;
+    const g_gamma = imageData.data[4 * baseIndex + 1] / 255;
+    const b_gamma = imageData.data[4 * baseIndex + 2] / 255;
+    const r_linear = gammaDecode(r_gamma);
+    const g_linear = gammaDecode(g_gamma);
+    const b_linear = gammaDecode(b_gamma);
+    result.X[baseIndex] = 0.4124 * r_linear + 0.3576 * g_linear + 0.1805 * b_linear;
+    result.Y[baseIndex] = 0.2126 * r_linear + 0.7152 * g_linear + 0.0722 * b_linear;
+    result.Z[baseIndex] = 0.0193 * r_linear + 0.1192 * g_linear + 0.9505 * b_linear;
   }
 }
